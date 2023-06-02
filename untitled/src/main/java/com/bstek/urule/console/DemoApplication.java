cdd9e0a143d50defedfdcb0cbc293d8ec690ce3b
songzhipeng <songzhipeng@loopin.group>
2023-06-02 10:34:14 +0800

    first commit

---
+++ untitled/src/main/java/com/bstek/urule/console/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.bstek.urule.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.bstek.urule.console.DemoApplication.class, args);
+	}
+}
